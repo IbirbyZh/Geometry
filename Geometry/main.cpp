1ba09b261704ff6ef86c23725abfe1f6414c6190
IbirbyZh <040796@gmail.com>
2016-02-27 19:11:07 +0300

    Initial Commit

---
+++ Geometry/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Geometry
+//
+//  Created by Шишкин Евгений on 27.02.16.
+//  Copyright © 2016 Шишкин Евгений. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
